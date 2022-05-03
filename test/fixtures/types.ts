83c884262b36e687ea2a6c1f68150aae78e902a7
Thomas Manning <thomasm@balena.io>
2022-05-03 08:27:08 +0000

    Fix template whitespace
    Change-type: patch

--- test/fixtures/types.ts
+++ test/fixtures/types.ts
@@ -4,12 +4,12 @@ export interface EmbryoData {
 	[k: string]: unknown;
 }
 
-export interface EmbryoContract extends Contract<EmbryoData > {
+export interface EmbryoContract extends Contract<EmbryoData> {
     type: 'embryo'
 }
 
-export interface EmbryoContractDefinition extends ContractDefinition<EmbryoData > {
-type: 'embryo'
+export interface EmbryoContractDefinition extends ContractDefinition<EmbryoData> {
+    type: 'embryo'
 }
 
 export interface LarvaData {
@@ -17,24 +17,24 @@ export interface LarvaData {
 	[k: string]: unknown;
 }
 
-export interface LarvaContract extends Contract<LarvaData > {
+export interface LarvaContract extends Contract<LarvaData> {
     type: 'larva'
 }
 
-export interface LarvaContractDefinition extends ContractDefinition<LarvaData > {
-type: 'larva'
+export interface LarvaContractDefinition extends ContractDefinition<LarvaData> {
+    type: 'larva'
 }
 
 export interface PupaData {
 	[k: string]: unknown;
 }
 
-export interface PupaContract extends Contract<PupaData > {
+export interface PupaContract extends Contract<PupaData> {
     type: 'pupa'
 }
 
-export interface PupaContractDefinition extends ContractDefinition<PupaData > {
-type: 'pupa'
+export interface PupaContractDefinition extends ContractDefinition<PupaData> {
+    type: 'pupa'
 }
 
 export interface AdultData {
@@ -42,11 +42,11 @@ export interface AdultData {
 	[k: string]: unknown;
 }
 
-export interface AdultContract extends Contract<AdultData > {
+export interface AdultContract extends Contract<AdultData> {
     type: 'adult'
 }
 
-export interface AdultContractDefinition extends ContractDefinition<AdultData > {
-type: 'adult'
+export interface AdultContractDefinition extends ContractDefinition<AdultData> {
+    type: 'adult'
 }
 
